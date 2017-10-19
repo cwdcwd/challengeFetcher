d656a6051b49721cac860f180fd164827f01df7a
CWD <cwd@cloudspokes.com>
2017-10-19 12:46:40 -0400

    switching to request promise native

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 'use strict';
 
-const _ = require('lodash');
 const fs = require('fs');
-const requestPromise = require('request-promise');
+const _ = require('lodash');
+const requestPromise = require('request-promise-native');
 const logger = require('winston');
 const kue = require('kue');
 const QueueHelper = require('./queueHelper');
 
 const queue = kue.createQueue();
+
+const challengeURL = 'https://api.topcoder.com/v3/challenges';
 const challengeListFile = 'prod_challenges.json';
+const resultsFolder = 'challenges';
 
 const qh = new QueueHelper(queue, {attempts: 5, delay: 1000}, (job, done) => {
   const challenge = job.data;
   logger.info(`calling for data for ${challenge.id}`);
-  // call out here
-  done();
+
+  const options = {
+    uri: `${challengeURL}/${challenge.id}`,
+    json: true
+  };
+
+  requestPromise(options).then((challengeDetail) => {
+
+    fs.writeFile(`./${resultsFolder}/${challenge.id}.json`, _.merge(challenge, challengeDetail), (writeErr) => {
+      if (writeErr) {
+        logger.error(writeErr);
+      }
+
+      done();
+    });
+  }).catch((callErr) => {
+    logger.error(callErr);
+    done();
+  });
 });
 
 
